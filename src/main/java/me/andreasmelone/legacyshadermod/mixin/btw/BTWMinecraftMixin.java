2e27a93c4ad762a1e9951cac037eb54b22231cc6
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-07-28 06:50:29 +0300

    Additionally, show GL version and vendor in BTW

--- src/main/java/me/andreasmelone/legacyshadermod/mixin/btw/BTWMinecraftMixin.java
+++ src/main/java/me/andreasmelone/legacyshadermod/mixin/btw/BTWMinecraftMixin.java
@@ -5,6 +5,7 @@ import api.client.debug.DebugRegistryUtils;
 import me.andreasmelone.legacyshadermod.client.Shaders;
 import net.minecraft.src.Minecraft;
 import net.minecraft.src.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,6 +27,9 @@ public class BTWMinecraftMixin {
         DebugInfoSection section = DebugRegistryUtils.registerSection(new ResourceLocation("shadersmod", "shaders"), DebugRegistryUtils.Side.RIGHT);
         section.addEntry((mc, isExtendedDebug) -> {
             return Optional.of("Shaderpack: §e" + Shaders.getShaderPack());
+        });
+        section.addEntry((mc, isExtendedDebug) -> {
+            return Optional.of(GL11.glGetString(GL11.GL_VERSION) + " §e" + GL11.glGetString(GL11.GL_VENDOR));
         });
     }
 }
